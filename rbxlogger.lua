2d1ff59fe79eeb0596f78000eabe219dcac5ec7c
PrintedScript <64908610+PrintedScript@users.noreply.github.com>
2021-12-02 23:41:20 +0800

    Generates Hash when the session ends

--- rbxlogger.lua
+++ rbxlogger.lua
@@ -3,14 +3,19 @@ task.wait(4)
 local Players = game:GetService("Players")
 local MarketplaceService = game:GetService("MarketplaceService")
 local LogFileName = tostring(game.PlaceId).."-"..math.floor(tick()).."-"..Players.LocalPlayer.Name..".txt"
+local Messages = ""
 local function GetTimeStamp()
     return os.date("%d/%m/%Y-%H:%M:%S") 
 end
 
+local TotalLines = 0
+
 local function GenerateLogMessage(Subject,Message)
+    Messages = Messages..Message
     return "[ "..GetTimeStamp().." ] - "..Subject.." # "..Message 
 end
 local function WriteToLog(message)
+    TotalLines += 1
     appendfile("RbxLogger/"..LogFileName,"\n"..message) 
 end
 local function WriteAsScript(Message)
@@ -29,13 +34,20 @@ WriteAsScript("Roblox User Logged in as "..GenerateUserInfo(Players.LocalPlayer)
 
 for _,v in pairs(Players:GetChildren()) do v.Chatted:connect(function(message) WriteToLog(GenerateLogMessage("MESSAGEEVENT",GenerateUserInfo(v).." said '"..message.."'")) end) end
 
-Players.PlayerAdded:Connect(function(player)
+local PlayerAddedConnection = Players.PlayerAdded:Connect(function(player)
     WriteToLog(GenerateLogMessage("PLAYEREVENT",GenerateUserInfo(player).." joined the game"))
     player.Chatted:connect(function(message)
         WriteToLog(GenerateLogMessage("MESSAGEEVENT",GenerateUserInfo(player).." said '"..message.."'"))
     end)
 end)
 
-Players.PlayerRemoving:connect(function(player)
+local PlayerLeavingConnection = Players.PlayerRemoving:connect(function(player)
     WriteToLog(GenerateLogMessage("PLAYEREVENT",GenerateUserInfo(player).." left the game"))
+    if player == Players.LocalPlayer then
+        PlayerLeavingConnection:Disconnect()
+        PlayerAddedConnection:Disconnect()
+        LogHash = syn.crypt.hash(tostring(game.PlaceId)..MarketplaceService:GetProductInfo(game.PlaceId).Name..game.JobId..tostring(game.PlaceVersion)..tostring(TotalLines+2)..Messages)
+        WriteAsScript("LOGHASH-SHA-384 "..tostring(LogHash))
+        WriteAsScript("RBX SESSION ENDED, TOTAL LOGS WRITTEN: "..tostring(TotalLines+1))
+    end
 end)
